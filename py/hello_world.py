838b1ee65179b11bcac18c1f33f20f66b107e22e
Arne Ewald <arne.ewald@nordakademie.de>
2025-02-12 19:57:18 +0100

    updated themes for nak and simpleplus

---
+++ py/hello_world.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib
+import matplotlib.pyplot as plt
+
+def main():
+    print("Hello, World!")
+    print("NumPy version:", np.__version__)
+    print("Pandas version:", pd.__version__)
+    print("Matplotlib version:", matplotlib.__version__)
+    print("Scikit-learn version:", sklearn.__version__)
+
+if __name__ == "__main__":
+    main()
